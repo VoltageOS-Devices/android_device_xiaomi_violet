d084761b8dbe72d5fc38470b02140a0cfeb8725a
Niranjan BR <niranjankannan2003@gmail.com>
2022-05-08 11:39:22 +0000

    violet: vendorsetup.sh: Track Kernel
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,7 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-12.0
 git clone https://github.com/mrfox2003/vendor_xiaomi_violet.git -b 12 vendor/xiaomi/violet
 
 # Kernel
-git clone https://github.com/raghavt20/kernel_sm6150.git -b 12 kernel/xiaomi/violet
+git clone https://github.com/mrfox2003/android_kernel_xiaomi_violet.git kernel/xiaomi/violet
 
 # ANX
 git clone -b twelve https://gitlab.com/madhavbiju/vendor_aeonax_anxcamera.git vendor/aeonax/ANXCamera
