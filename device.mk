b73e8520b95a15684c1dab26236b10f7485d94a4
Niranjan BR <niranjankannan2003@gmail.com>
2022-09-14 21:04:01 +0000

    violet: Build parts
    This reverts commit b6601151c6a2a164c26c7c42ee91dbfad327a142.

--- device.mk
+++ device.mk
@@ -122,6 +122,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
