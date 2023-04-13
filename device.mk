d5aca98577b262049e1e096e6d4c8f1be7a87f34
raghavt20 <raghavt20@aospa.co>
2023-04-13 18:04:06 +0530

    violet: Update blobs
    * vendor     from LA.UM.9.1.r1-12900-SMxxx0.0
    * system_ext from LA.QSSI.13.0.r1-09400.01-qssi.0

--- device.mk
+++ device.mk
@@ -39,7 +39,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl 
+    android.hardware.bluetooth.audio@2.1 \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
@@ -84,7 +86,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
